b71b59bcf4dff0ce1d71372b7d617b6b322dc6a0
Saul Njie <saulus92@gmail.com>
2021-10-21 11:49:13 +0200

    starting heart toggle and localstorage

--- js/index.js
+++ js/index.js
@@ -1,14 +1,13 @@
 import { fetchApi } from "./lib/fetchApi.js";
 import { filteringAnArray } from "./lib/filteringArray.js";
-// import {
-// 	saveToLocalStorage,
-// 	getStorageItem,
-// } from './libs/localStorageHelper.js';
+import {
+	saveToLocalStorage,
+	getStorageItem,
+} from "./lib/localStorageHelper.js";
 
 let data = await fetchApi("http://localhost:1337/articles");
 console.log(data);
 
-const loading = document.querySelector(".loading");
 
 const search = document.querySelector(".search");
 const searchResults = document.querySelector(".container");
@@ -52,16 +51,59 @@ search.onkeyup = function () {
     </div>
     </div>`;
   });
+
+  let likes = document.querySelectorAll('.fa-star');
+  // console.log('likes', likes);
+  
+  likes.forEach((element) => {
+    element.onclick = function () {
+      element.classList.toggle('fas');
+      console.log(element);
+      // Data sets are used to store extra data
+      // ======================================================
+      console.log(element.dataset.id);
+      console.log(element.dataset.title);
+      console.log(element.dataset.author);
+      // ======================================================
+  
+      // Creating an object to store to local storage
+      let localStorageObject = {
+        id: element.dataset.id,
+        price: element.dataset.title,
+        title: element.dataset.author,
+      };
+  
+      //Appending to what is in local storage
+      // With the code below we are adding to local storage, never removing anything
+      let favourites = getStorageItem('favourites');
+  
+      // Find Method in JS looks if something is in an array
+      // if it is then it will return it or else it will return undefined
+  
+      let isInStorage = favourites.find(
+        (productObject) => productObject.id === localStorageObject.id
+      );
+  
+      console.log('isInStorage', isInStorage);
+  
+      if (isInStorage === undefined) {
+        // Insert it into local storage
+        favourites.push(localStorageObject);
+        saveToLocalStorage('favourites', favourites);
+      } else {
+        // remove it from localstorage if its already there
+        let removedElementArray = favourites.filter(
+          (productObject) => productObject.id !== localStorageObject.id
+        );
+  
+        // console.log('removedElementArray', removedElementArray);
+  
+        saveToLocalStorage('favourites', removedElementArray);
+      }
+    };
+  });
+  
+
   console.log(filteredArray);
 
-
 };
-
-
-// let likes = document.querySelectorAll('.fa-star');
-// // console.log('likes', likes);
-
-// likes.forEach((element) => {
-// 	element.onclick = function () {
-// 		element.classList.toggle('fas');
-// };
